a3d6431352b73df6af4b7ff0e71e200900d99f3b
apple <1396539283@qq.com>
2016-11-28 18:29:45 +0800

    for 循环写的用户列表

--- DayangH/src/components/familyState/Body.js
+++ DayangH/src/components/familyState/Body.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {baseUrl} from '../../constants/Api'
 import {
     View,
     Text,
@@ -11,14 +12,47 @@ import {
 import UserListCell from './UserListCell'
 
 class Body extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dataSource: []
+        }
+    }
+    componentDidMount() {
+        //console.info('==Body==',this.props.DayangState)
+        var guardianId = this.props.DayangState.response.guardianId
+        var requestUrl = `${baseUrl}guardians/${guardianId}/users`
+        //console.info('==requestUrl',requestUrl)
+        var tokenStr = `Bear ${this.props.DayangState.response.token}`
+        //console.info('==token',tokenStr)
+        fetch(requestUrl, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": tokenStr
+            },
+        })
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.info("==recivedata==",responseData);
+            this.setState({
+                dataSource: responseData.users
+            })
+        })
+        .catch((error) => {
+            console.info("reciveerror",error);
+        });
+    }
     render() {
+        let userArray = this.state.dataSource
+        var result = userArray.map(function(value){
+            return <UserListCell userName={value.name} />
+        })
         return(
             <View style={styles.container}>
-                <Text style={styles.text}>
-                    {this.props.DayangState.response.token}
-                </Text>
-                <UserListCell userName="hhh">
-                </UserListCell>
+                <View style={styles.userList}>
+                {result}
+                </View>
             </View>
         )
     }
@@ -35,8 +69,11 @@ var styles = StyleSheet.create({
         marginTop: 64,
     },
     text: {
-        marginTop: 64,
+        marginTop: 164,
         color: 'red'
+    },
+    userList: {
+        marginTop: 64
     }
 })
 
